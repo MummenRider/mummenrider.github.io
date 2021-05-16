3a7c71a10deffdb574a5e564898629292ffc7350
MummenRider <clark.inocalla@hotmail.com>
2021-05-16 14:17:06 -0600

    Finished slide and fade in effect on scroll

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,9 @@
 /******************* Selectors *******************/
 const burger = document.querySelector(".burger");
 const navLinks = document.querySelector(".nav-links");
+const heroSection = document.querySelector(".hero-section");
+const sliders = document.querySelectorAll(".slide-in");
+const faders = document.querySelectorAll(".fade-in");
 
 /******************* Methods *******************/
 const burgerNav = () => {
@@ -8,5 +11,27 @@ const burgerNav = () => {
   burger.classList.toggle("line-effect");
 };
 
-/******************* Events *******************/
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -150px 0px",
+};
+
+const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) return;
+
+    entry.target.classList.add("appear");
+    appearOnScroll.unobserve(entry.target);
+  });
+}, appearOptions);
+
+/******************* Events/Obervers *******************/
 burger.addEventListener("click", burgerNav, false);
+
+sliders.forEach((slider) => {
+  appearOnScroll.observe(slider);
+});
+
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
+});
